5a8e8a7189639c547e93184a2855758d769f2a8a
Benjamin Pilgrim <benjamin.pilgrim@risqcapital.com>
2024-04-08 11:11:40 +0100

    chore: fix tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -11,51 +11,51 @@ from pyhcl2._ast import (
     Parenthesis,
     UnaryOp,
 )
-from pyhcl2.parse import parse_string
+from pyhcl2.parse import parse_module
 
 
 def test_can_parse_attribute_string() -> None:
-    assert parse_string('foo-bar = "Hello World!"') == Module(
+    assert parse_module('foo-bar = "Hello World!"') == Module(
         [Attribute("foo-bar", Literal("Hello World!"))]
     )
 
 
 def test_can_parse_attribute_float() -> None:
-    assert parse_string("foo-bar = 42.0") == Module(
+    assert parse_module("foo-bar = 42.0") == Module(
         [Attribute("foo-bar", Literal(42.0))]
     )
 
 
 def test_can_parse_attribute_null() -> None:
-    assert parse_string("foo-bar = null") == Module(
+    assert parse_module("foo-bar = null") == Module(
         [Attribute("foo-bar", Literal(None))]
     )
 
 
 def test_can_parse_empty_block() -> None:
-    assert parse_string("myblock {}") == Module([Block("myblock", [], [])])
+    assert parse_module("myblock {}") == Module([Block("myblock", [], [])])
 
 
 def test_can_parse_block_with_args() -> None:
-    assert parse_string('myblock "argument1" "argument2" {}') == Module(
+    assert parse_module('myblock "argument1" "argument2" {}') == Module(
         [Block("myblock", [Literal("argument1"), Literal("argument2")], [])]
     )
 
 
 def test_can_parse_block_with_args_as_identifiers() -> None:
-    assert parse_string("myblock argument1 argument2 {}") == Module(
+    assert parse_module("myblock argument1 argument2 {}") == Module(
         [Block("myblock", [Identifier("argument1"), Identifier("argument2")], [])]
     )
 
 
 def test_can_parse_read_from_attributes() -> None:
-    assert parse_string("value = foo") == Module(
+    assert parse_module("value = foo") == Module(
         [Attribute("value", Identifier("foo"))]
     )
-    assert parse_string("value = foo.bar") == Module(
+    assert parse_module("value = foo.bar") == Module(
         [Attribute("value", GetAttr(Identifier("foo"), GetAttrKey(Identifier("bar"))))]
     )
-    assert parse_string("value = foo.bar.baz") == Module(
+    assert parse_module("value = foo.bar.baz") == Module(
         [
             Attribute(
                 "value",
@@ -66,13 +66,13 @@ def test_can_parse_read_from_attributes() -> None:
             )
         ]
     )
-    assert parse_string('value = "42".bar') == Module(
+    assert parse_module('value = "42".bar') == Module(
         [Attribute("value", GetAttr(Literal("42"), GetAttrKey(Identifier("bar"))))]
     )
 
 
 def test_can_parse_binary_operator() -> None:
-    assert parse_string("value = 1 + (addend * 2) - 1 ") == Module(
+    assert parse_module("value = 1 + (addend * 2) - 1 ") == Module(
         [
             Attribute(
                 "value",
@@ -95,7 +95,7 @@ def test_can_parse_binary_operator() -> None:
         ]
     )
 
-    assert parse_string("value = 1 + addend * 2 - 1 ") == Module(
+    assert parse_module("value = 1 + addend * 2 - 1 ") == Module(
         [
             Attribute(
                 "value",
@@ -114,12 +114,12 @@ def test_can_parse_binary_operator() -> None:
 
 
 def test_can_parse_unary_operator() -> None:
-    assert parse_string("value = !addend") == Module(
+    assert parse_module("value = !addend") == Module(
         [Attribute("value", UnaryOp("!", Identifier("addend")))]
     )
 
 
 def test_can_parse_function_call() -> None:
-    assert parse_string("value = foo()") == Module(
+    assert parse_module("value = foo()") == Module(
         [Attribute("value", FunctionCall(Identifier("foo"), []))]
     )
