f88833e9a8d0ca31e87d103fe8841449045ff8c3
Benjamin Pilgrim <benjamin.pilgrim@risqcapital.com>
2024-10-11 15:12:32 +0100

    feat: Allow installing

--- src/pyhcl2/repl.py
+++ src/pyhcl2/repl.py
@@ -2,17 +2,16 @@ from pathlib import Path
 
 import rich
 from prompt_toolkit import PromptSession
-from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
-from rich.console import Group, NewLine
+from prompt_toolkit.history import FileHistory
+from rich.console import NewLine
 
 from pyhcl2.eval import Evaluator, EvaluationScope
 from pyhcl2.parse import parse_expr_or_attribute
-from pyhcl2.pymiette import Diagnostic, LabeledSpan
+from pyhcl2.pymiette import Diagnostic
 from pyhcl2.rich_utils import Inline
-from pyhcl2.values import Unresolved
 
-if __name__ == "__main__":
+def main() -> None:
     scope = EvaluationScope()
     session: PromptSession = PromptSession(history=FileHistory(Path.home() / ".pyhcl2_history"))
     try:
@@ -30,7 +29,10 @@ if __name__ == "__main__":
                 result = Evaluator().eval(ast, scope)
                 rich.print(Inline(result.resolve().raise_on_unresolved(), NewLine()))
             except Diagnostic as diagnostic:
-                    rich.print(diagnostic.with_source_code(text))
+                rich.print(diagnostic.with_source_code(text))
 
     except KeyboardInterrupt:
         pass
+
+if __name__ == "__main__":
+    main()
